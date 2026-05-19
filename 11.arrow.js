21e2a9533c4338b478f12eb7952c32d4bdcb2e3c
SantzxB <caiosantanaborges37@gmail.com>
2026-05-19 16:44:41 -0300

    Create 11.arrow.js

---
+++ 11.arrow.js
@@ -0,0 +1,27 @@
+// Arrow Function(função flecha)
+
+const somar = (a,b) => {
+    return a + b
+}
+const soma = somar(2,3)
+console.log(`Soma: ${soma}`)
+
+const subtrair = (a,b) =>{
+    return a - b 
+}
+const subtracao =  subtrair(2,3)
+console.log(`Subtração: ${subtracao}`)
+
+const multiplicar = (a,b) => {
+    return a * b
+}
+
+const multiplicacao = multiplicar(2,3)
+console.log(`Multiplicação: ${multiplicacao}`)
+
+const dividir = (a,b) => {
+    return a / b
+}
+
+const divisão = dividir(2,3)
+console.log(`Divisão: ${divisão}`)
