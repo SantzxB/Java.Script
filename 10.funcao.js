ee408272caa80a50442170c5b33cf5d0332afa41
SantzxB <caiosantanaborges37@gmail.com>
2026-05-19 16:44:22 -0300

    Create 10.funcao.js

---
+++ 10.funcao.js
@@ -0,0 +1,26 @@
+// Função
+
+function somar(a,b){
+    return a + b
+}
+const soma = somar (2,3)
+console.log(`Soma:  ${soma}`)
+
+function subtrair(a,b) {
+    return a - b
+}
+const subtracao =  subtrair(7,2)
+console.log(`Subtração: ${subtracao}`)
+
+function multiplicar (a,b){
+    return a * b
+}
+const multiplicacao = multiplicar(5,7)
+console.log(`Multiplicação: ${multiplicacao}`)
+
+function dividir(a,b){
+    return a / b
+}
+
+const divisao = dividir(80,4)
+console.log(`Divisão: ${divisao}`)
