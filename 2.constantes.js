9fb6a9fa1fddb0b6e18d46c6acb517cde92807d7
SantzxB <caiosantanaborges37@gmail.com>
2026-05-19 15:29:33 -0300

    Create 2.constantes.js

---
+++ 2.constantes.js
@@ -0,0 +1,13 @@
+// Constantes.
+
+const idade = 20
+const peso  = 89.9
+const altura = 1.71
+const telefone = 7198283724
+//  Peso, altura, telefone
+
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(`Telefone: ${telefone}`)
+
