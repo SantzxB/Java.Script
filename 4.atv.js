c86e1f88e6336bf294d5d8a50de8ea3953cdf187
SantzxB <caiosantanaborges37@gmail.com>
2026-05-20 16:22:24 -0300

    Create 4.atv.js

---
+++ 4.atv.js
@@ -0,0 +1,11 @@
+// Quarta Atividade de JS sem retorno
+
+const resposta  = (a,b) => {
+    if (a == b)
+        resultado = a + b
+    else
+        resultado = a * b
+    console.log(`Valor de C: ${resultado}`)
+    
+}
+resposta(8,8)
